4c9a562f738c99f00b1f17d0157740076624e4d5
yurma96 <yurmaafonso25@hotmail.com>
2026-05-03 02:53:54 +0100

    Criação da página suporte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div><h1>Projeto Integrador</h1></div>
+    <main className="suporte-page">
+      <section className="suporte-header">
+        <h1>Suporte</h1>
+        <p>Relate problemas ou bugs encontrados na plataforma.</p>
+      </section>
+
+      <section className="form-card">
+        <form>
+          <label>Nome Completo</label>
+          <input type="text" placeholder="Como devemos lhe chamar?" />
+
+          <label>E-mail Académico</label>
+          <input type="email" placeholder="seu.nome@instituicao.edu" />
+
+          <label>Assunto</label>
+          <input type="text" placeholder="Sobre o que deseja falar?" />
+
+          <label>Descrição do Problema</label>
+          <textarea placeholder="Descreva o ocorrido..." />
+
+          <button type="submit">▷ Enviar Relatório</button>
+        </form>
+      </section>
+    </main>
   );
 }
