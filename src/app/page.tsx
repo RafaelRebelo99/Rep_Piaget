1804e7a780c1639242c8b29a7cea2e41a57e1506
RafaelRebelo99 <120263354+RafaelRebelo99@users.noreply.github.com>
2026-05-06 15:28:09 +0100

    SuggestCourseCard.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import SuggestCourseCard from "@/components/SuggestCourseCard";
 
 export default function Home() {
   return (
-    <div><h1>Projeto Integrador</h1></div>
-  );
+
+    <div>
+      <h1>Projeto Integrador</h1>
+    </div>
+  )
 }
---
+++ src/components/SuggestCourseCard.tsx
@@ -0,0 +1,13 @@
+export default function SuggestCourseCard() {
+    return (
+        <div className="border-2 border-dashed border-gray-300 rounded-xl bg-white flex flex-col items-center justify-center p-6 h-full ">
+       <div className="w-12 h-12 rounded-full border-2 border-gray-300 flex items-center justify-center text-gray-400 text-2xl">
+        +
+     </div>
+    <p className="text-gray-600 font-semibold mt-3">Sugerir um curso</p>
+   <p className="text-gray-400 text-sm text-center mt-1">Diz-nos o que gostarias de adicionar</p>
+</div>
+
+    )
+
+}
