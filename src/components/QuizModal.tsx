351bfc4d465e4b5ec0c70b54fdf1759f8a308643
RafaelRebelo99 <120263354+RafaelRebelo99@users.noreply.github.com>
2026-06-12 16:11:29 +0100

    Fixed line 39 issue in QuizzModal.tsx

--- src/components/QuizModal.tsx
+++ src/components/QuizModal.tsx
@@ -36,7 +36,8 @@ export default function QuizModal({ disciplineName, materials, onClose }: QuizMo
   function toggleMaterial(id: string) {
     setSelected(prev => {
       const next = new Set(prev)
-      next.has(id) ? next.delete(id) : next.add(id)
+      if (next.has(id)) next.delete(id)
+      else next.add(id)
       return next
     })
   }
